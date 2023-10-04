b9d86fc89d5c2d8f256005d324b7e6efc6d025ab
priyahsi <144327837+priyahsi@users.noreply.github.com>
2023-10-04 22:12:06 +0530

    created

---
+++ Javascript/day1.js
@@ -0,0 +1,45 @@
+var name = "priya"
+var abb = "have a good day"
+var bvv = "good morning"
+var num = "12345"
+var val = "false"
+
+console.log(typeof ("priya"))
+console.log(name)
+console.log(abb)
+console.log(typeof ('have a good day'))
+console.log(bvv)
+console.log(typeof (`good morning`))
+console.log(num)
+console.log(typeof ("12345"))
+console.log(val)
+console.log(typeof ("false"))
+
+
+
+var subject = false; 
+console.log(subject)
+console.log(typeof (subject))
+
+
+
+
+var number1 = 23;
+var number2 = 98;
+var addition = number1 + number2;
+console.log(addition, "- addition")
+
+var x = 45;
+var y = 56;
+var substraction = x-y;
+console.log(substraction, "- substraction")
+
+var r = 46;
+var s = 90;
+var multiplication = r*s;
+console.log(multiplication, "- multiplication")
+
+var div1= 30;
+var div2 = 60;
+var division = div1/div2;
+console.log(division, "- division")
