0db5d3c4f5eef1a8bb814d20145c6b11a5f56f7f
priyahsi <144327837+priyahsi@users.noreply.github.com>
2023-10-05 23:11:13 +0530

    created

---
+++ Day2js/day2.js
@@ -0,0 +1,17 @@
+var num1 = 5;
+if(num1 == 6){
+    console.log("its a similar value")
+} else if(num1 < 6 && num1 <= 6){
+    console.log("5 is less than than 6 ")
+} else if(num1 <= 6 || num1 == 6){
+    console.log("5 is less than 6 ")
+}
+else{
+    console.log("Its not similar values")
+}
+console.log(9 == 9 && 2 == 2)
+console.log(7 == 15 && 5 == 5)
+console.log(7 == 23 && 2 == 34)
+s
+console.log(3 == 1 || 4 == 2)
+console.log(15 == 15 || 2 == 23)
