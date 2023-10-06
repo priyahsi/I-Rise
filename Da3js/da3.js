8ee365bb8426021c63c83abbaf7267963be19618
priyahsi <144327837+priyahsi@users.noreply.github.com>
2023-10-06 09:31:57 +0530

    created

---
+++ Da3js/da3.js
@@ -0,0 +1,9 @@
+var number = 45632;
+if(number % 2 == 0){
+    console.log(`the ${number} is even`)
+}
+else{
+    console.log(`the ${number} is odd`)
+}
+// var myNumber
+// console.log(typeof(myNumber))
