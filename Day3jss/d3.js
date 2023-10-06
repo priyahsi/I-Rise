cf1b61304e08e3b16086a97cb7bb42a7840995e4
priyahsi <144327837+priyahsi@users.noreply.github.com>
2023-10-06 13:25:22 +0530

    created

---
+++ Day3jss/d3.js
@@ -0,0 +1,15 @@
+var num1 = 24;
+var num2 = 88;
+var num3 = 97;
+  if(num1 % 2 == 0){
+    if(num2 % 2 == 0){
+            console.log(`the ${num2} is even `)
+    }else{
+        console.log(`the ${num2} is odd`)
+    }
+    if (num3 % 2 == 0){
+        console.log(`the ${num3} is even`)
+    }else{
+        console.log(`the ${num3} is odd`)
+    }
+  }
