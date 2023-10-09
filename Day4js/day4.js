d3401f9dc041cf8c1d8224b417712c1e0c293507
priyahsi <144327837+priyahsi@users.noreply.github.com>
2023-10-09 15:45:24 +0530

    created

---
+++ Day4js/day4.js
@@ -0,0 +1,50 @@
+
+ for (var i = 0; i < 10; i++) {
+     console.log(i)
+ }
+
+
+
+ for (var i = 0; i <= 10; i++) {
+     console.log(i)
+ }
+
+
+
+
+
+for (var n = 1; n <= 15; n++) {
+    console.log(n)
+}
+
+
+ 
+
+function myName() {
+    console.log("Hello")
+    console.log("Good morning")
+}
+myName();
+
+function Mul(num) {
+    console.log(num * num)
+}
+Mul(2);
+
+function Arth(val1, val2) {
+    console.log(val1 + val2, "-addition")
+    console.log(val1 - val2, "-subt")
+    console.log(val1 * val2, "-multi")
+    console.log(val1 / val2, "-divid")
+}
+Arth(64,8);
+
+
+function divid(x, y) {
+    var div = x / y;
+    console.log(div)
+    console.log(Math.floor(div))
+    console.log(Math.ceil(div))
+}
+
+divid(67,5);
