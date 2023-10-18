a103f9712a7ff433402ddd4104dcdae4181b23e9
priyahsi <144327837+priyahsi@users.noreply.github.com>
2023-10-18 08:57:11 +0530

    created

--- day9js/d9.js
+++ day9js/d9.js
@@ -12,13 +12,6 @@ numbers2.sort((a, b) => b - a);
 console.log(numbers2)
 numbers2.sort((a, b) => a - b);
 console.log(numbers2)
-
-// function myFunction() { }
-//   () => 
-
-// JSON.stringify - convert JS data into JSON 
-// JSON.parse     - convert JSON data into JS
-
 var myObject = { iriseStudent: "hello" }
 
 var students = JSON.stringify(myObject)
